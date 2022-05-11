996823cee5f2279c42f541f169c28ae186786abb
lkdev778 <91744831+LKDEV778@users.noreply.github.com>
2022-05-11 18:47:49 +0200

    Update guns-c.lua

--- GiveweaponSpawncar/guns-c.lua
+++ GiveweaponSpawncar/guns-c.lua
@@ -1,8 +1,9 @@
+--WEAPONS--
+
 RegisterCommand("clearweapons", function()
     RemoveAllPedWeapons(GetPlayerPed(-1), true)
     notify("~r~Cleared All Weaopons")
 end, false)
-
 
 Citizen.CreateThread(function()
     local h_key = 74
@@ -16,4 +17,20 @@ Citizen.CreateThread(function()
             alert("~b~Given Weapon with ~INPUT_VEH_HEADLIGHT~")
         end
     end
+end)
+
+--VEHICLES--
+RegisterCommand('spawnveh', function(src, args)
+    RequestModel(args[1])
+    while not HasModelLoaded(args[1]) do 
+        Wait(0)
+    end
+    local Player = PlayerPedId()
+    local pCoords = GetEntityCoords(Player) --vector3
+    local pHeading = GetEntityHeading(Player) --Heading
+    if IsPedInAnyVehicle(Player, false) then 
+        DeleteVehicle(GetVehiclePedIsIn(Player))
+    end
+    local car = CreateVehicle(GetHashKey(args[1]), pCoords, pHeading, true, false)
+    TaskWarpPedIntoVehicle(Player, car, -1)
 end)
